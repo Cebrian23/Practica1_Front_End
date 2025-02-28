85f66e110388bb34949c5ca4a472c933eb750b66
Miguel <miguelangel5cebrian@gmail.com>
2025-02-28 02:22:45 +0100

    Segundo Commit

--- islands/Module.tsx
+++ islands/Module.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from "preact";
 import Button from "./Button.tsx";
 import { useState } from "preact/hooks";
-import { Handlers,FreshContext } from "$fresh/server.ts";
+import { FreshContext, Handlers } from "$fresh/server.ts";
 
 type props = {
     name: string,
@@ -21,18 +21,45 @@ export const handler: Handlers = {
 }
 
 const Modulo: FunctionComponent<props & Contenido> = (Props) => {
+    let info = "";
     const { name } = Props;
-    const [ string, setText ] = useState<string>("");
     const [ number, setContador ] = useState<number>(Props.contador);
+    const [ string, setText ] = useState<string>("");
     
     return (
         <>
-            <div class="oculto" hidden={number%2 === 1}>
-                <header>
-                    <b class = "left">{name}</b>
+            <div class="oculto" hidden={number === 1}>
+                <section class="espaciado">
+                    <b>{name}</b>
                     <Button variant ="Mostrar_mas" onClick={() => setContador(number + 1)}>Mostrar mas</Button>
-                </header>
+                </section>
             </div>
+            <div class="ampliado" hidden={number === 0}>
+                <section class="espaciado">
+                    <b>{name}</b>
+                    <Button variant = "Mostrar_menos" onClick={() => setContador(number - 1)}>Mostrar menos</Button>
+                </section>
+                <hr/>
+                <section class="columnas">
+                    <i>{Props.texto}</i>
+                    <input type="text" id="itexto" name="texto" value={Props.texto}></input>
+                    <button type="submit" onClick={
+                        () => {
+                            if(Props.texto){
+                                setText(string+Props.texto);
+                            }
+                        }
+                    }>Input de texto</button>
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default Modulo;
+
+/**
+ * 
             <div class="ampliado" hidden={number%2 === 0}>
                 <header>
                     <b class = "left">{name}</b>
@@ -52,8 +79,4 @@ const Modulo: FunctionComponent<props & Contenido> = (Props) => {
                         >Input de texto</button>
                     </section>
             </div>
-        </>
-    );
-}
-
-export default Modulo;
+ */
